ff341676ba744ed27867968a7aa9a6d3836d41cb
ww24 <ww24gm@gmail.com>
2013-10-29 01:32:38 +0900

    fix logs type-check

--- models/logs.js
+++ models/logs.js
@@ -155,9 +155,9 @@ module.exports = function () {
     });
   };
   model.events.set = function (sid, log_info, callback) {
-    if (log_info.user_id_hex !== "string")
+    if (typeof log_info.user_id_hex !== "string")
       throw new TypeError("log_info.user_id_hex must be string");
-    if (log_info.index !== "number")
+    if (typeof log_info.index !== "number")
       throw new TypeError("log_info.index must be number");
 
     client.hset("CallSid", sid, JSON.stringify(log_info), function (err, reply) {
